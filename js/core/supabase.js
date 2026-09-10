b123533dbfa016b702d3f6795eff18ed12699802
web-studiosite <felisbertouele7@gmail.com>
2026-09-11 00:59:49 +0200

    Update supabase.js

--- js/core/supabase.js
+++ js/core/supabase.js
@@ -9,10 +9,10 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 // 1. Defina a URL do seu projeto Supabase:
-export const SUPABASE_URL = window.__ENV__?.SUPABASE_URL || 'https://seu-projeto.supabase.co';
+export const SUPABASE_URL = window.__ENV__?.SUPABASE_URL || 'https://vjajbqiobmemdohzpjjm.supabase.co';
 
 // 2. Defina a chave pública (anon key) do seu projeto Supabase:
-export const SUPABASE_ANON_KEY = window.__ENV__?.SUPABASE_ANON_KEY || 'sua-chave-anon-publica-do-supabase';
+export const SUPABASE_ANON_KEY = window.__ENV__?.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqYWpicWlvYm1lbWRvaHpwamptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNzI1OTcsImV4cCI6MjEwNDY0ODU5N30.JwHjFWI7ncH_BV9J4n1pkJkRmqVXVial8xgjh0t6-JI';
 
 // 3. Inicialização do cliente Supabase
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
