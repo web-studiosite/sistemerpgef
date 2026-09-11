0f354ef969e9a4fff43e8b562cc3e4ea91cec468
web-studiosite <felisbertouele7@gmail.com>
2026-09-11 03:11:30 +0200

    Update supabase.js

--- js/core/supabase.js
+++ js/core/supabase.js
@@ -9,11 +9,10 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 // 1. Defina a URL do seu projeto Supabase:
-export const SUPABASE_URL = window.__ENV__?.SUPABASE_URL || 'https://vjajbqiobmemdohzpjjm.supabase.co';
+export const SUPABASE_URL = window.__ENV__?.SUPABASE_URL || 'https://utrbgcjcozrmgunvwysk.supabase.co';
 
 // 2. Defina a chave pública (anon key) do seu projeto Supabase:
-export const SUPABASE_ANON_KEY = window.__ENV__?.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZqYWpicWlvYm1lbWRvaHpwamptIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNzI1OTcsImV4cCI6MjEwNDY0ODU5N30.JwHjFWI7ncH_BV9J4n1pkJkRmqVXVial8xgjh0t6-JI';
-
+export const SUPABASE_ANON_KEY = window.__ENV__?.SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0cmJnY2pjb3pybWd1bnZ3eXNrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkwNzcxNDcsImV4cCI6MjEwNDY1MzE0N30.br6y9zxabYsFhZy6oj9lcEDqmvH5NJ1XGVwdvjJpyqQ';
 // 3. Inicialização do cliente Supabase
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
